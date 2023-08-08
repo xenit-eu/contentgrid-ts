6411272226cd950077fcb47a2280d00d7582f3b8
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-08-08 13:23:43 +0200

    Move babel, jest, rollup & typescript configs to top level folder

--- packages/uri-template/babel.config.mjs
+++ packages/uri-template/babel.config.mjs
@@ -1,8 +1,1 @@
-/** @type {import('@babel/core').TransformOptions} */
-export default {
-    "presets": ["@babel/preset-env", "@babel/preset-typescript"],
-    "plugins": ["@babel/plugin-transform-runtime"],
-    "targets": {
-        "browsers": "defaults"
-    }
-}
+export { default } from "../../config/babel.config.mjs";
