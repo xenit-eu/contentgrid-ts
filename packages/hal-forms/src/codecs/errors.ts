27629b3c3394b52f3b1ea75ed92398d758fe2396
Lars Vierbergen <lars.vierbergen@xenit.eu>
2024-03-26 11:54:04 +0100

    Extend HalFormsCodecNotAvailableError to contain template name & method in exception message

--- packages/hal-forms/src/codecs/errors.ts
+++ packages/hal-forms/src/codecs/errors.ts
@@ -9,7 +9,7 @@ export class HalFormsCodecNotAvailableError extends HalFormsTemplateError {
     public constructor(
         template: HalFormsTemplate<any>
     ) {
-        super(template.name, `no encoder available for content type "${template.contentType}"`);
+        super(template.name, `no encoder available for template "${template.name}" (method="${template.request.method}"; contentType="${template.contentType}")`);
         Object.setPrototypeOf(this, new.target.prototype);
         this.name = HalFormsCodecNotAvailableError.name;
     }
